6c51e939be12bc27d3138e3284e73beb632fd0ec
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2022-01-10 08:33:20 +0700

    Add empty index page

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
-fn main() {
-    println!("Hello, world!");
+#[macro_use]
+extern crate dotenv;
+
+use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+use std::env;
+use dotenv::dotenv;
+
+#[get("/")]
+async fn index() -> impl Responder {
+    HttpResponse::Ok().body(";)")
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    dotenv().ok();
+
+    let service_bind = env::var("SERVICE_BIND")
+        .expect("SERVICE_BIND must be set");
+        
+    HttpServer::new(|| {
+        App::new()
+            .service(index)
+    })
+    .bind(service_bind)?
+    .run()
+    .await
 }

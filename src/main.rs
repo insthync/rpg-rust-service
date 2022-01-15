74e3af81357116f5c6f44a0103afec86156c9a2d
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2022-01-15 23:17:08 +0700

    Try connect to the database

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
 #[macro_use]
 extern crate dotenv;
+#[macro_use]
+extern crate rbatis;
 
 use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
 use std::env;
 use dotenv::dotenv;
+use rbatis::rbatis::Rbatis;
 
 #[get("/")]
 async fn index() -> impl Responder {
@@ -12,11 +15,20 @@ async fn index() -> impl Responder {
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    // Read config
     dotenv().ok();
-
     let service_bind = env::var("SERVICE_BIND")
-        .expect("SERVICE_BIND must be set");
+        .expect("`SERVICE_BIND` must be set");
+    let database_url = env::var("DATABASE_URL")
+        .expect("`DATABASE_URL` must be set");
         
+    // Enable log crate to show sql logs
+    fast_log::init_log("requests.log", log::Level::Info, None, true);
+    // Initialize rbatis. May use `lazy_static` crate to define rbatis as a global variable because rbatis is thread safe
+    let rb = Rbatis::new();
+    // Connect to database  
+    rb.link(&database_url).await.unwrap();
+
     HttpServer::new(|| {
         App::new()
             .service(index)
